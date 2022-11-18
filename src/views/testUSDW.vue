73569b98001d1ab6ce2f7b0765a4422d3b3b9fcd
luozy <997088290@qq.com>
2022-11-18 10:31:35 +0800

    update

--- src/views/testUSDW.vue
+++ src/views/testUSDW.vue
@@ -12,13 +12,13 @@
     <div class="row">
       <el-col>代币总供应量(_totalSupply)</el-col>
       <el-col><el-button @click="_totalSupply">TotalSupply</el-button></el-col>
-      <el-col>{{ decimal?totalSupply/10**decimal:totalSupply }}</el-col>
+      <el-col>{{ decimal?totalSupply/decimalNumber:totalSupply }}</el-col>
     </div>
     <div class="row">
       <el-col>余额查询(balanceOf)</el-col>
       <el-input v-model="search1" placeholder="查询对象地址"></el-input>
       <el-col><el-button @click="balanceOf">balanceOf</el-button></el-col>
-      <el-col>{{ decimal?balance/10**decimal:balance }}</el-col>
+      <el-col>{{ decimal?balance/decimalNumber:balance }}</el-col>
     </div>
     <div class="row">
       <el-col>精度(decimals)</el-col>
